ab367952c5c1ce09e536664091c2e4d307e014a1
serhatozles <serhatozles@gmail.com>
2014-11-27 14:54:34 +0200

    developing

--- elFinder.php
+++ elFinder.php
@@ -2,13 +2,36 @@
 
 namespace serhatozles\elfinder;
 
+use yii\helpers\Json;
+
 /**
  * This is just an example.
  */
-class elFinder extends \yii\base\Widget
-{
-    public function run()
-    {
-        return "Hello!";
+class elFinder extends \yii\base\Widget {
+
+    public $ajax;
+    public $language;
+    public $options = [];
+
+    public function run() {
+
+	if (!empty($this->ajax)) {
+
+	    elFinderAsset::register($this->getView());
+
+	    $this->options['url'] = $this->ajax;
+
+	    if (!empty($this->language)) {
+
+		$this->options['lang'] = $this->language;
+
+		$this->getView()->registerJsFile(__DIR__ . "/assets/js/i18n/elfinder." . $this->language . ".js");
+	    }
+
+	    $this->getView()->registerJs("$('#elfinder').elfinder(" . Json::encode($this->options) . ").elfinder('instance');");
+
+	    return "Hello!";
+	}
     }
+
 }
---
+++ elFinderAsset.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Serhat OZLES, nippy.in, 2014
+ * @package yii2-elfinder
+ * @version 1.0.0
+ */
+
+namespace serhatozles\elfinder;
+
+/**
+ * elFinder bundle for \kartik\elfinder\elFinder.
+ *
+ * @author Serhat OZLES <serhatozles@gmail.com>
+ * @since 1.0
+ */
+class elFinderAsset extends \yii\web\AssetBundle
+{
+    
+    public $css = [
+        'css/elfinder.min.css',
+        'css/theme.css',
+    ];
+    public $js = [
+	'js/elfinder.min.js',
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset'
+    ];
+
+    public function init()
+    {
+        $this->sourcePath = __DIR__."/assets";
+        parent::init();
+    }
+
+}
