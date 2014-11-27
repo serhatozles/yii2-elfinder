03e4b9aaea165b5e916f433d3462483da65ca8e6
serhatozles <serhatozles@gmail.com>
2014-11-27 06:14:26 +0200

    develop

---
+++ elFinder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace serhatozles\elfinder;
+
+/**
+ * This is just an example.
+ */
+class elFinder extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
